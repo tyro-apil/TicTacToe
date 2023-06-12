3e7adaa5440cb4743adb838e61d6ab1c248321d3
apil <078bct017.apil@pcampus.edu.np>
2023-06-12 08:43:04 +0545

    Add required data and member functions

--- inc/ui.hpp
+++ inc/ui.hpp
@@ -12,12 +12,24 @@ class board
 {
     private:
         grid elements[3][5];
-
+        grid winner;
+        char playerA_mark, playerB_mark, empty_mark, gap_mark;
+        int turn_count;
+        
     public:
         board(): elements({grid::empty, grid::gap, grid::empty, grid::gap, grid::empty},
                           {grid::empty, grid::gap, grid::empty, grid::gap, grid::empty},
-                          {grid::empty, grid::gap, grid::empty, grid::gap, grid::empty}){ } //Constructor
-        
+                          {grid::empty, grid::gap, grid::empty, grid::gap, grid::empty}),
+                 winner(grid::empty),
+                 playerA_mark('O'), playerB_mark('X'), empty_mark('_'), gap_mark(' '),
+                 turn_count(0){ }//Constructor
+        void display_grid();
+        grid prompt(int);
+        bool check_diagonal(grid);
+        bool check_row(grid);
+        bool check_col(grid);
+        bool check_win(bool);
+        void display_win(grid, bool);
 };
 
 #endif  //_GRID_HPP
