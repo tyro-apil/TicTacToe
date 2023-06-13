29e693b1a0fc6b0314525bbb8efcdafe06c20701
apil <078bct017.apil@pcampus.edu.np>
2023-06-13 21:49:38 +0545

    Add main function call

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,25 @@
 //main function for cpp project
 
-#include<iostream>
-#include<iomanip>
+#include "ui.hpp"
 
+int board::turn_count = 0;
 int main()
 {
-
+    grid player;
+    bool win;
+    board tictactoe;
+    while(board::turn_count <= 9)
+    {
+        if(tictactoe.winner != grid::empty)
+        {
+            return;
+        }
+        tictactoe.display_grid();
+        player = tictactoe.prompt();
+        tictactoe.set_mark(player);
+        win = tictactoe.check_win(player);
+        tictactoe.display_win(player, win);
+    }
+    std::cout<<"### Game ends in TIE ###"<<std::endl;
     return 0;
 }
