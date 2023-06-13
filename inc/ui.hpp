b8b94155f34fbe3f333ee86f50c4887cdfc6eebc
apil <078bct017.apil@pcampus.edu.np>
2023-06-13 17:28:13 +0545

    Change turn_count to static variable

--- inc/ui.hpp
+++ inc/ui.hpp
@@ -3,6 +3,8 @@
 #ifndef _UI_HPP
 #define _UI_HPP
 
+#include <iostream>
+
 enum class grid { empty = 0,
                   gap,
                   playerA,
@@ -14,22 +16,22 @@ class board
         grid elements[3][5];
         grid winner;
         char playerA_mark, playerB_mark, empty_mark, gap_mark;
-        int turn_count;
+        static int turn_count;
         
     public:
         board(): elements({{grid::empty, grid::gap, grid::empty, grid::gap, grid::empty},
                           {grid::empty, grid::gap, grid::empty, grid::gap, grid::empty},
                           {grid::empty, grid::gap, grid::empty, grid::gap, grid::empty}}),
                  winner(grid::empty),
-                 playerA_mark('O'), playerB_mark('X'), empty_mark('_'), gap_mark(' '),
-                 turn_count(0){ }//Constructor
+                 playerA_mark('O'), playerB_mark('X'), empty_mark('_'), gap_mark(' ')
+                 { }//Constructor
         void display_grid();
-        grid prompt(int);
+        grid prompt(int);                   //Whose turn
         bool check_diagonal(grid);
         bool check_row(grid);
         bool check_col(grid);
-        bool check_win(bool);
-        void display_win(grid, bool);
+        bool check_win(bool);               //Check all conditions for win
+        void display_win(grid, bool);       //Check win for prompt player
 };
 
 #endif  //_GRID_HPP
